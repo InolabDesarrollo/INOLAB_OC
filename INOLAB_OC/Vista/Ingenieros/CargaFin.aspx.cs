c5cd4bf34a88de75b1f99b86b33847d571afb1e4
InolabDesarrollo <134556950+InolabDesarrollo@users.noreply.github.com>
2023-07-26 13:28:24 -0600

    Agrego clase CorreoElectronico para refactorizar clase CargaFin y agregar instancias de la clase CorreoElectronico en metodos de envio de correo, de esta forma los metodos son mas eficientes

--- INOLAB_OC/Vista/Ingenieros/CargaFin.aspx.cs
+++ INOLAB_OC/Vista/Ingenieros/CargaFin.aspx.cs
@@ -19,6 +19,7 @@ using INOLAB_OC.Modelo.Browser;
 using INOLAB_OC.Modelo.Inolab;
 using INOLAB_OC.Controlador.Ingenieros;
 using DocumentFormat.OpenXml.Drawing.Charts;
+using INOLAB_OC.Vista.Ingenieros;
 
 namespace INOLAB_OC
 {
@@ -63,21 +64,16 @@ namespace INOLAB_OC
             if (!Page.IsPostBack)
             {
                 ReportViewer1.ServerReport.ReportServerCredentials = new MyReportServerCredentials();
-                // Set the processing mode for the ReportViewer to Remote
                 ReportViewer1.ProcessingMode = ProcessingMode.Remote;
-
                 ServerReport serverReport = ReportViewer1.ServerReport;
 
-                // Set the report server URL and report path
                 serverReport.ReportServerUrl = new Uri("http://INOLABSERVER01/Reportes_Inolab");
                 serverReport.ReportPath = "/OC/FSR Servicio v2";
 
-                // Create the sales order number report parameter
                 ReportParameter salesOrderNumber = new ReportParameter();
                 salesOrderNumber.Name = "folio";
                 salesOrderNumber.Values.Add(Session["folio_p"].ToString());
 
-                // Set the report parameters for the report
                 ReportViewer1.ServerReport.SetParameters(new ReportParameter[] { salesOrderNumber });
                 ReportViewer1.ShowParameterPrompts = false;
             }
@@ -104,9 +100,7 @@ namespace INOLAB_OC
                      By reading the information on demand instead of 
                      storing it, the credentials will not be stored in 
                      session, reducing the vulnerable surface area to the
-                     Web.config file, which can be secured with an ACL.
-
-                     User name */
+                     Web.config file, which can be secured with an ACL.*/
                     string userName =
                         ConfigurationManager.AppSettings
                             ["MyReportViewerUser"];
@@ -114,7 +108,6 @@ namespace INOLAB_OC
                     if (string.IsNullOrEmpty(userName))
                         throw new Exception("Missing user name from web.config file");
 
-                    // Password
                     string password =
                         ConfigurationManager.AppSettings
                             ["MyReportViewerPassword"];
@@ -122,7 +115,6 @@ namespace INOLAB_OC
                     if (string.IsNullOrEmpty(password))
                         throw new Exception("Missing password from web.config file");
 
-                    // Domain
                     string domain =
                         ConfigurationManager.AppSettings
                             ["MyReportViewerDomain"];
@@ -139,8 +131,6 @@ namespace INOLAB_OC
                 userName = null;
                 password = null;
                 authority = null;
-
-                // Not using form credentials
                 return false;
             }
         }
@@ -150,6 +140,7 @@ namespace INOLAB_OC
             string idUsuario = Session["idUsuario"].ToString();
             Timer1.Enabled = false;
             string correoDeCliente = controladorFSR.consultarMailDeFolioServicio(folioFSR);
+
             if(!correoDeCliente.Equals("") || correoDeCliente != null)
             {
                 string correoElectronicoCliente = correoDeCliente;
@@ -160,7 +151,7 @@ namespace INOLAB_OC
                 actualizarEstatusDeCierreDeActividadEnSap();
 
                 ReportViewer1.ServerReport.Refresh();
-                string path = CreatePDF(Session["folio_p"].ToString());
+                string path = crearPDF(Session["folio_p"].ToString());
                 notificarAlAsesorDeVentasDatosDeFolioServicio();
                 verificarElTipoDeContrato(path, correoElectronicoCliente);
                 envioDeCorreoElectronicoACliente(path);
@@ -184,18 +175,15 @@ namespace INOLAB_OC
             try
             {
                 controladorOCLG.actualizarEstatusFolio(folioFSR);
-
                 controladorSCL5.actualizarValorDeCampo("U_ESTATUS", "Finalizado", folioFSR);
-                string callId = controladorSCL5.seleccionarValorDeCampo("SrvcCallId", folioFSR);
 
+                string callId = controladorSCL5.seleccionarValorDeCampo("SrvcCallId", folioFSR);
                 string numeroDeValoresEnEstatus = controladorSCL5.consultarNumeroDeFoliosPorCallId(callId.ToString());
                 int numeroDeValoresEnSLC5 = controladorSCL5.contarFilasDeTablaPorCallId(callId.ToString());
-
                 bool HayFoliosConEstatusDiferenteDeFinalizado = controladorSCL5.verificarSiHayFoliosConEstatusDiferenteDeFinalizado(numeroDeValoresEnSLC5, callId.ToString());
 
                 controladorOSCL = new C_OSCL(repositorioOSCL, int.Parse(callId));
                 controladorOSCL.verificarSiSeCierraLaLLamada(numeroDeValoresEnEstatus, HayFoliosConEstatusDiferenteDeFinalizado);
-
             }
             catch (Exception er)
             {
@@ -203,15 +191,13 @@ namespace INOLAB_OC
             }
         }
 
-        private string CreatePDF(string fileName)
+        private string crearPDF(string fileName)
         {
             Warning[] warnings;
             string[] streamIds;
             string mimeType = string.Empty;
             string encoding = string.Empty;
             string extension = string.Empty;
-
-            //Setup the report viewer object and get the array of bytes 
             byte[] bytes = ReportViewer1.ServerReport.Render("PDF", null, out mimeType, out encoding, out extension, out streamIds, out warnings);
 
             // Now that you have all the bytes representing the PDF report, buffer it and send it to the client.  
@@ -246,31 +232,20 @@ namespace INOLAB_OC
             {
                 MailAddress correoElectronicoEmisor = new MailAddress("notificaciones@inolab.com");
                 MailAddress correoElectronicoReceptor = new MailAddress(mailDeAsesorDeVentas);
-                MailMessage mensajeDeCorreoElectronico = new MailMessage(correoElectronicoEmisor, correoElectronicoReceptor);
+                MailMessage mensaje = new MailMessage(correoElectronicoEmisor, correoElectronicoReceptor);
 
-                mensajeDeCorreoElectronico.Bcc.Add("luisrosales@inolab.com, omarflores@inolab.com, carlosflores@inolab.com");
-                mensajeDeCorreoElectronico.Body = cuerpoDelCorreoElectronicoParaVentas(Session["folio_p"].ToString());
-                mensajeDeCorreoElectronico.IsBodyHtml = true;
-                mensajeDeCorreoElectronico.Subject = asunto;
+                mensaje.Bcc.Add("luisrosales@inolab.com, omarflores@inolab.com, carlosflores@inolab.com");
+                mensaje.Body = cuerpoDelCorreoElectronicoParaVentas(Session["folio_p"].ToString());
+                mensaje.IsBodyHtml = true;
+                mensaje.Subject = asunto;
 
-
-                SmtpClient configuracionCorreoElectronico = new SmtpClient();
-                configuracionCorreoElectronico.Port = 1025;
-                configuracionCorreoElectronico.Host = "smtp.inolab.com";
-                configuracionCorreoElectronico.EnableSsl = false;
-                configuracionCorreoElectronico.DeliveryMethod = SmtpDeliveryMethod.Network;
-                configuracionCorreoElectronico.UseDefaultCredentials = false;
-                configuracionCorreoElectronico.Credentials = new System.Net.NetworkCredential("notificaciones@inolab.com", "Notificaciones2021*");
-                configuracionCorreoElectronico.Send(mensajeDeCorreoElectronico);
-
-                mensajeDeCorreoElectronico.Dispose();
-                configuracionCorreoElectronico.Dispose();
+                CorreoElectronico correo = new CorreoElectronico("notificaciones@inolab.com", "Notificaciones2021*");
+                correo.enviar(mensaje);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
-            }
-                
+            }              
         }
         private string cuerpoDelCorreoElectronicoParaVentas(string folio) 
         {
@@ -282,29 +257,14 @@ namespace INOLAB_OC
                 reader.Dispose();
             }
 
-            string observacionesDelFolio = controladorFSR.consultarValorDeCampoTop(folioFSR,
-                "Observaciones");
-
-
-            string llamada = "Interna";
-            try
-            {
-                llamada = controladorSCL5.seleccionarValorDeCampoTop("SrvcCallId", folioFSR);
-            }
-            catch (Exception es)
-            {
-                Console.Write(es.ToString());
-            }
-
+            string observacionesDelFolio = controladorFSR.consultarValorDeCampoTop(folioFSR, "Observaciones");
+            string llamada = definirElTipoDeLLamada(folioFSR);
             string cliente = controladorFSR.consultarValorDeCampoTop(folioFSR, "Cliente");
             string equipo = controladorFSR.consultarValorDeCampoTop(folioFSR, "Equipo");
-
             string tipoDeServicio = controladorV_FSR.consultarValorDeCampoTop("TipoServicio", folioFSR);
             string ingeniero = controladorV_FSR.consultarValorDeCampoTop("Ingeniero", folioFSR);
-
             string actividad = controladorV_FSR.consultarValorDeCampoTop("Actividad", folioFSR);
             string OrdenVenta = controladorV_FSR.consultarValorDeCampoTop("OC", folioFSR);
-
 
             cuerpoDelCorreo = cuerpoDelCorreo.Replace("{folio}", folio);
             cuerpoDelCorreo = cuerpoDelCorreo.Replace("{slogan}", "data:image/png;base64," + convertirImagenAStringBase64(Server.MapPath("/Imagenes/slogan.png")));
@@ -318,6 +278,20 @@ namespace INOLAB_OC
             cuerpoDelCorreo = cuerpoDelCorreo.Replace("{ingeniero}", ingeniero);
             return cuerpoDelCorreo;
         }
+
+        private string definirElTipoDeLLamada(string idFolioFSR)
+        {
+            string llamada = "Interna";
+            try
+            {
+                llamada = controladorSCL5.seleccionarValorDeCampoTop("SrvcCallId", idFolioFSR);
+            }
+            catch (Exception es)
+            {
+                Console.Write(es.ToString());
+            }
+            return llamada;
+        }
         
         private void envioDeCorreoElectronicoACliente(string filepath)
         {
@@ -327,31 +301,24 @@ namespace INOLAB_OC
             string contraseña = "Notificaciones2021*";
             try
             {
-                MailMessage mailMessage = new MailMessage();
-                mailMessage.From = new MailAddress(correoElectronicoEmisor);
-                mailMessage.Bcc.Add(correosElectronicosReceptores);
-                mailMessage.Subject = folioDeServico;
-                mailMessage.Body = cuerpoDelCorreoElectronicoParaCliente(Session["folio_p"].ToString(), "cliente");
-                mailMessage.IsBodyHtml = true;
+                MailMessage mensaje = new MailMessage();
+                mensaje.From = new MailAddress(correoElectronicoEmisor);
+                mensaje.Bcc.Add(correosElectronicosReceptores);
+                mensaje.Subject = folioDeServico;
+                mensaje.Body = cuerpoDelCorreoElectronicoParaCliente(Session["folio_p"].ToString(), "cliente");
+                mensaje.IsBodyHtml = true;
 
                 Attachment attach = new Attachment(filepath);
-                mailMessage.Attachments.Add(attach);
-
-                SmtpClient smtpClient = new SmtpClient("smtp.inolab.com");
-                smtpClient.EnableSsl = false;
-                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
-                smtpClient.UseDefaultCredentials = false;
-                smtpClient.Port = 587;
-                smtpClient.Credentials = new NetworkCredential(correoElectronicoEmisor, contraseña);
-                smtpClient.Send(mailMessage);
-                smtpClient.Dispose();
+                mensaje.Attachments.Add(attach);
+   
+                CorreoElectronico correoElectronico = new CorreoElectronico(correoElectronicoEmisor, contraseña);
+                correoElectronico.enviar(mensaje);
             }
             catch(Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }           
         }
-
         private string cuerpoDelCorreoElectronicoParaCliente(string folioDeServicio, string cliente)
         {
             string cuerpoDelCorreo = string.Empty;
@@ -372,12 +339,10 @@ namespace INOLAB_OC
         {
             string idContrato = controladorFSR.consultarValorDeCampoTop(Session["folio_p"].ToString(), "IdT_Contrato");
             string servicioPuntual = "7";
-
             if (idContrato.Equals(servicioPuntual))
             {
                 enviarCorreoElectronicoParaFacturacion(path);
             }
-
         }
 
         protected static string convertirImagenAStringBase64(string imgPath)
@@ -392,28 +357,19 @@ namespace INOLAB_OC
             try
             {
                 MailAddress correoElectronicoEmisor = new MailAddress("notificaciones@inolab.com");
-                MailAddress correoElectronicoReceptor = new MailAddress("facturacion@inolab.com"); //facturacion@inolab.com"
-                MailMessage mensajeDelCorreo = new MailMessage(correoElectronicoEmisor, correoElectronicoReceptor);
+                MailAddress correoElectronicoReceptor = new MailAddress("facturacion@inolab.com"); 
+                MailMessage mensaje = new MailMessage(correoElectronicoEmisor, correoElectronicoReceptor);
 
-                mensajeDelCorreo.Bcc.Add("carlosflores@inolab.com");// luisrosales@inolab.com
-                mensajeDelCorreo.Body = cuerpoDelCorreoElectronicoParaFacturacion(Session["folio_p"].ToString());
-                mensajeDelCorreo.IsBodyHtml = true;
-                mensajeDelCorreo.Subject = asuntoDelCorreoElectronico;
+                mensaje.Bcc.Add("luisrosales@inolab.com, omarflores@inolab.com");
+                mensaje.Body = cuerpoDelCorreoElectronicoParaFacturacion(Session["folio_p"].ToString());
+                mensaje.IsBodyHtml = true;
+                mensaje.Subject = asuntoDelCorreoElectronico;
 
                 Attachment folioQueSeEnviara = new Attachment(filepath);
-                mensajeDelCorreo.Attachments.Add(folioQueSeEnviara);
-
-                SmtpClient configuracionEmisor = new SmtpClient();
-
-                configuracionEmisor.Port = 1025;
-                configuracionEmisor.Host = "smtp.inolab.com";
-                configuracionEmisor.EnableSsl = false;
-                configuracionEmisor.DeliveryMethod = SmtpDeliveryMethod.Network;
-                configuracionEmisor.UseDefaultCredentials = false;
-                configuracionEmisor.Credentials = new System.Net.NetworkCredential("notificaciones@inolab.com", "Notificaciones2021*");
-                configuracionEmisor.Send(mensajeDelCorreo);
-                mensajeDelCorreo.Dispose();
-                configuracionEmisor.Dispose();
+                mensaje.Attachments.Add(folioQueSeEnviara);
+            
+                CorreoElectronico correo = new CorreoElectronico("notificaciones@inolab.com", "Notificaciones2021*");
+                correo.enviar(mensaje);
             }
             catch(Exception ex) 
             {
@@ -430,19 +386,7 @@ namespace INOLAB_OC
                 cuerpoDelCorreo = reader.ReadToEnd();
                 reader.Dispose();
             }
-
-            //En caso de ser llamada interna, no tendra datos en SAP (Este campo queda como "Interno")
-            string tipoDeLLamada = "Interna";
-            try
-            {
-                tipoDeLLamada = controladorSCL5.seleccionarValorDeCampoTop("SrvcCallId", Session["folio_p"].ToString());
-            }
-            catch (Exception es)
-            {
-                Console.Write(es.ToString());        
-            }
-            
-            //Obtencion de datos para el correo
+            string tipoDeLLamada = definirElTipoDeLLamada(Session["folio_p"].ToString());
             string cliente = controladorFSR.consultarValorDeCampoTop("Cliente", folioFSR);
             string actividad =  controladorFSR.consultarValorDeCampoTop("Actividad", folioFSR);
             string OrdenVenta = controladorV_FSR.consultarValorDeCampoTop("OC", folioFSR);
